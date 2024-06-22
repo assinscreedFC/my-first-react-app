72a0c40545a7140cfd8920778bcf2935d4e54ddd
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-06-23 00:38:22 +0100

    downlloaad pdf

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,15 +1,38 @@
+import { useRef } from "react";
 import BarCv from "./BarCv.jsx";
 import CorpCv from "./CorpCv.jsx";
-
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 function Cv(params) {
+
+    const pidif =useRef();
+    const handleDownloadPdf =  () => {
+        const input = pidif.current;
+        html2canvas(input).then((canvas)=>{
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF("p","mm","a4",true);
+            const pdfWidth=pdf.internal.pageSize.getWidth();
+            const pdfHeight=pdf.internal.pageSize.getHeight();
+            const imgWidth=canvas.width;
+            const imgHeight=canvas.height;
+            const ratio=Math.min(pdfWidth/imgWidth,pdfHeight/imgHeight);
+            const imgX=(pdfWidth-imgWidth*ratio)/2;
+            const imgY=30;
+            pdf.addImage(imgData,"PNG",imgX,imgY,imgWidth*ratio,imgHeight*ratio);
+            pdf.save("cv.pdf");
+        })
+    }
     return (<>
     
-        <div className=" bg-slate-100 h-svh  w-svw flex flex-col ">
+        <div ref={pidif} className=" bg-slate-100 h-svh  w-svw flex flex-col ">
     
             <BarCv/>
             <CorpCv/>
-        </div>
+             <button className="w-5 " onClick={handleDownloadPdf}> Download</button>
     
+        </div>
+
+       
     </>);
 }
 
